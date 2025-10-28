dec68b74e12049462dc5d84ab0208a6b5ff97cd3
randallpaguio <tookiechips@gmail.com>
2025-10-28 08:05:02 -0700

    Cleanup: Remove debug console logs

--- src/components/GameControls.tsx
+++ src/components/GameControls.tsx
@@ -27,12 +27,8 @@ const GameControls: React.FC<GameControlsProps> = ({
   resetTimer,
   adminLocked
 }) => {
-  // Debug: Log what we receive
-  console.log('🎮 [GameControls] Rendered with:', { adminLocked, isMatchStarted, showControls });
-  
   // If admin is locked, don't show any controls at all
   if (adminLocked === true) {
-    console.log('🎮 [GameControls] Admin is LOCKED, returning null');
     return null;
   }
 
