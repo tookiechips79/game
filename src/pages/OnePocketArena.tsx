04395a9465ee460e46abe63d853e6c8bb50886d0
randallpaguio <tookiechips@gmail.com>
2025-11-02 01:31:31 -0800

    Change One Pocket Arena background to light blue

--- src/pages/OnePocketArena.tsx
+++ src/pages/OnePocketArena.tsx
@@ -956,7 +956,7 @@ const OnePocketArena = () => {
 
 
   return (
-    <div className="min-h-screen bg-purple-950 p-4 md:p-8 pt-32 relative">
+    <div className="min-h-screen p-4 md:p-8 pt-32 relative" style={{ backgroundColor: '#e0f7ff' }}>
       
       <div className="max-w-6xl mx-auto relative z-10">
         <UserWidgetsContainer 
