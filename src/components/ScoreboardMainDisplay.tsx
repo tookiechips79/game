0ef133849578f43a0bd444edeff20097dc350739
randallpaguio <tookiechips@gmail.com>
2025-11-15 10:33:46 -0800

    VISUAL: Move Tony's picture (Team B avatar) to the right
    PROBLEM:
       - Tony's picture was positioned at 55% horizontally
       - User requested to move it further right
    
    SOLUTION:
       Updated Team B player image background-position:
    
       OLD: position="55% 55%"
       NEW: position="70% 55%"
    
       Moves image 15% further to the right while keeping vertical position
    
    CHANGES:
       1. ScoreboardMainDisplay.tsx: Updated Team B position prop
    
    RESULT:
       ✓ Tony's picture now positioned more to the right
       ✓ Better visual balance on Team B side
       ✓ Cleaner scoreboard appearance

--- src/components/ScoreboardMainDisplay.tsx
+++ src/components/ScoreboardMainDisplay.tsx
@@ -194,7 +194,7 @@ const ScoreboardMainDisplay: React.FC<ScoreboardMainDisplayProps> = ({
                 onNameChange={onTeamBNameChange}
                 adminLocked={adminLocked}
                 playerImageUrl={teamBPlayerImageUrl}
-                position="55% 55%"
+                position="70% 55%"
                 showBallCount={showBallCount}
               />
             </div>
